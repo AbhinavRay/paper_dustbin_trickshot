373bbc837cc610299add9649c6494d720dcc8bfe
AbhinavRay <68186989+AbhinavRay@users.noreply.github.com>
2020-09-05 23:57:13 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+var dustbin
 
 function preload(){
 	dustbinImg = loadImage("images/dustbin.png");
@@ -18,30 +19,44 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color("yellow");
 
-	boxPart1 = createSprite(647, height-135,175,10);
+	boxPart1 = createSprite(650, height-53,115,20);
 	boxPart1.shapeColor = color("white");
-	boxPart1.addImage(dustbinImg)
-	boxPart1.scale = 0.55
+	//dustbin.addImage(dustbinImg)
+	boxPart1.scale = 1.35
+	boxPart1.visible = false
+
+	dustbin = createSprite(650, height-145,115,20)
+	dustbin.addImage(dustbinImg)
 
 	ball = createSprite(width/10, height-60,20,20);
 	ball.shapeColor = color("Purple");
 	ball.addImage(paperImg)
 	ball.scale = 0.3
 
-	boxPart2 = createSprite(558, height-65,10,50);
+	boxPart2 = createSprite(558, height-135,30,190);
 	boxPart2.shapeColor = color("white");
 	boxPart2.visible = false
 
-	boxPart3 = createSprite(742, height-65,10,50);
+	boxPart3 = createSprite(742, height-135,30,190);
 	boxPart3.shapeColor = color("white");
 	boxPart3.visible = false
 
 	Engine.run(engine);
 
 	ball.body = Bodies.rectangle(width/10, height-50,20,20,{isStatic: false,restitution: 0.3,friction: 0.5,density: 1.2})
+	World.add(world,ball.body)
 
-	boxPart = Bodies.rectangle(650, height-45,175,10, {isStatic:true} );
- 	World.add(world, boxPart);
+	ground = Bodies.rectangle(width/2, height-35, width,10,{isStatic:true})
+	World.add(world,ground)
+
+	dustbin = Bodies.rectangle(650, height-45,175,10, {isStatic:true} );
+	World.add(world, dustbin);
+	 
+	boxPart2 = Bodies.rectangle(650, height-45,175,10, {isStatic:true} );
+	World.add(world, boxPart2);
+
+	boxPart3 = Bodies.rectangle(650, height-45,175,10, {isStatic:true} );
+ 	World.add(world, boxPart3);
   
 }
 
@@ -49,16 +64,12 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  ball.x=ball.body.position.x
+  ball.body.position.x = ball.x
   drawSprites();
-  keyPressed();
-  if(keyCode === UP_ARROW){
-	  matter.Body.applyForce(ball.body,)
-  }
-}
-
-function keyPressed(){
-	if(keyPressed(upArrow)){
-		Matter.Body.applyForce(ball.body,ball.body.position,{x:85,y:-85})
+  //keyPressed();
+  ball.velocityY = 5
+  if(keyDown(UP_ARROW)){
+		ball.velocityX = 45
+		ball.velocityY = -45
 	}
 }
